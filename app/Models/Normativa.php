0fe6350ad3eab9fd057cb988c725613a11bad911
juan ramon <juanramoncornejo@gmail.com>
2023-11-08 19:46:05 -0300

    configuracion del modelo

--- app/Models/Normativa.php
+++ app/Models/Normativa.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Normativa extends Model
 {
     use HasFactory;
+    protected $fillable = ['titulo','tipo_documento','archivo'];
+    public function scopeBuscar($query, $buscar)
+    {
+        if ($buscar === '') {
+            return;
+        }
+        return $query->where('titulo', 'like', '%' . $buscar . '%');
+    }
 }
