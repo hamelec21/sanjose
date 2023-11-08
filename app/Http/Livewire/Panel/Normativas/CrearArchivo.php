27199f18e62b0a14e34678f5ba86c2ed7f0b6c4b
juan ramon <juanramoncornejo@gmail.com>
2023-11-08 19:51:54 -0300

    controllador crear archivo listo

--- app/Http/Livewire/Panel/Normativas/CrearArchivo.php
+++ app/Http/Livewire/Panel/Normativas/CrearArchivo.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Livewire\Panel\Normativas;
 
+use App\Models\Normativa;
 use Livewire\Component;
-
+use Livewire\WithFileUploads;
 class CrearArchivo extends Component
 {
+    use WithFileUploads;
+
+    public $titulo, $tipo_documento,$archivo; // variables publicas del formularios
+
+    protected $rules = [
+        'titulo' => 'required|min:10|max:140',
+        'tipo_documento' => 'required',
+        'archivo'=>'required',
+    ];
+
+    public function save()
+    {
+        $this-> validate();
+        $archivo = $this->archivo->store('public/documentos');
+
+        Normativa::create([
+            'titulo' => $this->titulo,
+            'tipo_documento' => $this->tipo_documento,
+            'archivo' => $archivo,
+        ]);
+
+        $this->reset(['titulo','tipo_documento','archivo']);
+        $this->emit('render');
+        $this->emit('insert', 'El Registro fue Creado Exitosamente');
+        return redirect()->route('show-archivos');
+    }
     public function render()
     {
         return view('livewire.panel.normativas.crear-archivo');
